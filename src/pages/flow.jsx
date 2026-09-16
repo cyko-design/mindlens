750b16c0702baf70fe1e92012196fc68554661b0
Charles Yuen <97659964+cyko-design@users.noreply.github.com>
2026-09-16 20:00:34 +0800

    Update flow.jsx
    added line break after copy link on Desktop handoff

--- src/pages/flow.jsx
+++ src/pages/flow.jsx
@@ -713,7 +713,7 @@ export function Desktop() {
             }}
           >
             Copy link / 复制链接
-          </Button>
+          </Button><br /><br />
           <a href={REPOSITORY_URL} target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
